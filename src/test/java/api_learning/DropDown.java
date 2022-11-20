9c216faa875f9ee09d7a80a6c100be980f06bd3f
chamvu080497 <chamvudqh@gmail.com>
2022-11-21 06:32:06 +0700

    lesson_18

---
+++ src/test/java/api_learning/DropDown.java
@@ -0,0 +1,43 @@
+package api_learning;
+
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import support.ui.SelectEx;
+import url.Urls;
+
+public class DropDown {
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.getChromeDriver();
+
+        try{
+            // Navigate to the dropdown page
+            driver.get(Urls.BASE_URL.concat(Urls.DROPDOWN_SLUG));
+
+            //Locate the select tag / Dropdown
+            WebElement dropdownElem = driver.findElement(By.cssSelector("#dropdown"));
+            SelectEx select = new SelectEx(dropdownElem);
+
+            //Select by visible text
+            select.selectOption01();
+            Thread.sleep(1000);
+
+            //select by index
+             select.selectByIndex(1);
+             Thread.sleep(2000);
+
+            //select by value
+            select.selectByValue("1");
+
+            //debug purpose value
+            Thread.sleep(2000);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        driver.quit();
+    }
+}
---
+++ src/test/java/api_learning/IFrame.java
@@ -0,0 +1,44 @@
+package api_learning;
+
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import support.ui.SelectEx;
+import url.Urls;
+
+public class IFrame {
+    //iframe[^id="mce]
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.getChromeDriver();
+
+        try{
+            // Navigate to the dropdown page
+            driver.get(Urls.BASE_URL.concat(Urls.IFRAME_SLUG));
+
+            //Locate the iframe
+            WebElement IFrameElem = driver.findElement(By.cssSelector("iframe[^id='mce']"));
+            SelectEx select = new SelectEx(IFrameElem);
+            //switch to the iframe
+            driver.switchTo().frame(IFrameElem);
+
+            //input the text into input text area
+            WebElement edittorInputElem = driver.findElement(By.cssSelector("#tinymce"));
+            edittorInputElem.click();
+            edittorInputElem.clear();
+            edittorInputElem.sendKeys("The is the new value ");
+
+            //switch to the parent iframe
+            driver.switchTo().defaultContent();
+            System.out.println(driver.findElement(By.partialLinkText("Selenium")).getAttribute("href"));
+
+            //debug purpose value
+            Thread.sleep(2000);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        driver.quit();
+    }
+}
